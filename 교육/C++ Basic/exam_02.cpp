6dcddf64629961ac8db7ca460692d58c8a4a563e
lodos14 <81665608+lodos14@users.noreply.github.com>
2021-05-31 15:13:35 +0900

    Add files via upload

---
+++ 교육/C++ Basic/exam_02.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#define TEXT "Welcome to C++ programming!!"
+using namespace std;
+
+int main() {
+	/*
+	  namespace : 이름공간
+	  std 객체(instance 객체)
+	  :: => scope 연산자(소유 연산자), ~꺼
+	  std:: => input : cin(=console input), cout
+	  */
+	cout << TEXT;
+	
+	return 0;
+}
---
+++ 교육/C++ Basic/exam_17.cpp
@@ -0,0 +1,33 @@
+/* 
+ 대입연산자(=)와 배정대입연산자(+=, -=, /=, *=, %=)
+ 증감연산자 : 1씩 증가와 감소를 수행하는 연산자, 단항연산자
+
+ - 증가: ++
+ - 감소 : --
+ - 우선순위 : 전위증감연산/ 후위증감연산
+   ++ 변수, --변수
+   변수++, 변수--
+   최우선연산자 : (), [배열], . (direct연산자), 단항연산자(전위증감연산자, 논리부정(!), 비트부정(~), 부호연산(+ / -))
+ */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	//int num1 = 8;
+	//int num2 = 8;
+	//int num3 = 8;
+	int num1, num2, num3;
+    num1 = num2 = num3 = 8; // 연산방향 <-
+
+	num1 = num1 + 5;
+	num2 += 5; // num2 = num2 + 5
+	num3 =+ 5; // 단순대입연산자 5를 num3에 넣어라
+
+	cout << " num1 = " << num2 << endl;
+	cout << " num2 = " << num2 << endl;
+	cout << " num3 = " << num3 << endl;
+
+
+	return 0;
+}
